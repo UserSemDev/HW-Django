70b301a2791676e95e10128605eed60ff90e70ca
Sem <semseveryanin@gmail.com>
2024-04-06 23:40:09 +0300

    refactoring code

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
+from catalog.apps import CatalogConfig
 from catalog.views import home, contacts, product_info, add_product, products_page
 
+app_name = CatalogConfig.name
+
 urlpatterns = [
-    path('', home),
-    path('<int:pk>', products_page),
-    path('contacts/', contacts),
-    path('product/<int:pk>', product_info),
-    path('add_product/', add_product),
+    path('', home, name='home'),
+    path('<int:pk>', products_page, name='products_page'),
+    path('contacts/', contacts, name='contacts'),
+    path('product/<int:pk>', product_info, name='product_info'),
+    path('add_product/', add_product, name='add_product'),
 ]
